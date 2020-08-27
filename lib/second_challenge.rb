fcace346bc357b81f56827130e71ca11861cc10f
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-27 18:46:33 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,5 @@
 def second_challenge
-  epic_tragedy = {
-    
+ epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -18,7 +17,6 @@ def second_challenge
           {name: "Steven", age: "30", attitude: "confused"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
-  }
-
-  }
+   }
+}
 end
