41482231f0bc20ce019d5ece597278ffe6cd1104
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-27 18:43:44 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => []]}}
+      :heroine_friends => []}}
 
   
 end
